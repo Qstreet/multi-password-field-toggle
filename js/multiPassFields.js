db3a13f94fbbaa5d75d8a1ebc78780fd2e81ad20
Qstreet <git@qstreet.org>
2019-10-17 05:12:04 -0400

    Add refactored code

--- js/multiPassFields.js
+++ js/multiPassFields.js
@@ -8,17 +8,41 @@
 (function () {
   'use strict';
 
-  var checkBox = document.querySelector('#show-passwords')
-  var inputNodesArray = Array.prototype.slice.call(document.querySelectorAll('input'))
+  // var checkBox = document.querySelector('#show-passwords')
+  // var inputNodesArray = Array.prototype.slice.call(document.querySelectorAll('input'))
 
-  if (checkBox) {
+  // if (checkBox) {
+
+    //   checkBox.addEventListener('change', function (event) {
+    //     inputNodesArray.forEach(function (item, index) {
+    //       if (item.type === 'checkbox') return;
+    //       else if (item.type === 'password') {
+    //         item.type = 'text'
+    //       } else { item.type = 'password' }
+    //     })
+    //   }, false)
+
+    // REFACTOR
+    var checkBox = document.querySelector('#show-passwords')
+    var inputNodesArray = Array.prototype.slice.call(document.querySelectorAll('[type="password"]'))
+
+    // check to see if checkBox exists in DOM
+    if (checkBox) {
+
+    // Listener event and callback
     checkBox.addEventListener('change', function (event) {
-      inputNodesArray.forEach(function (item, index) {
-        if (item.type === 'checkbox') return;
-        else if (item.type === 'password') {
-          item.type = 'text'
-        } else { item.type = 'password' }
+
+      // there IS a NodeList.forEach but browswer support is sketchy
+      // either use a polyfill or convert to proper array as here
+      inputNodesArray.forEach(function (password) {
+        if (checkBox.checked) {
+          password.type = 'text'
+        } else {
+          password.type = 'password'
+        }
       })
+
     }, false)
+
   }
 })();
